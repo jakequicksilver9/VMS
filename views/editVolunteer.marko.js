86582cf68d35ec4a3751d09e93ce143b66f29643
jakequicksilver9 <jakequicksilver9@gmail.com>
2020-08-17 20:38:26 -0400

    form stuff

--- views/editVolunteer.marko.js
+++ views/editVolunteer.marko.js
@@ -22,49 +22,49 @@ function render(input, out, __component, component, state) {
     marko_escapeXml(input.greeting) +
     "</h1><h1>Edit Volunteer</h1><form" +
     marko_attr("action", "/editVolunteer/" + (input.id == null ? "" : input.id)) +
-    " method=post name=editVolunteer><label for=first>First name: </label><input type=text id=first name=firstname" +
+    " method=post name=editVolunteer><div class=\"row justify-content-center\"><div class=col-6><div class=form-group><div class=row><label class=form-label for=first>First name: </label><input class=form-control type=text id=first name=firstname" +
     marko_attr("value", input.volunteer.firstname) +
-    "><br><br><label for=last>Last name: </label><input type=text id=last name=lastname" +
+    "><br><br></div></div><div class=form-group><div class=row><label class=form-label for=last>Last name: </label><input class=form-control type=text id=last name=lastname" +
     marko_attr("value", input.volunteer.lastname) +
-    "><br><br><label for=userName>User name: </label><input type=text id=userName name=username" +
+    "><br><br></div></div><div class=form-group><div class=row><label class=form-label for=userName>User name: </label><input class=form-control type=text id=userName name=username" +
     marko_attr("value", input.volunteer.username) +
-    "><br><br><label for=password>Password: </label><input type=password id=password name=password" +
+    "><br><br></div></div><div class=form-group><div class=row><label class=form-label for=password>Password: </label><input class=form-control type=password id=password name=password" +
     marko_attr("value", input.volunteer.password) +
-    "><br><br><label for>Perfered Volunteer centers: </label><textarea rows=3 cols=40 name=centers id" +
+    "><br><br></div></div><div class=form-group><div class=row><label class=form-label for>Perfered Volunteer centers: </label><textarea class=form-control rows=3 cols=40 name=centers id" +
     marko_attr("value", input.volunteer.centers) +
-    "></textarea><br><br><label for>Skills/Interests: </label><textarea rows=3 cols=40 name=skills id" +
+    "></textarea><br><br></div></div><div class=form-group><div class=row><label class=form-label for>Skills/Interests: </label><textarea class=form-control rows=3 cols=40 name=skills id" +
     marko_attr("value", input.volunteer.skills) +
-    "></textarea><br><br><label for>Availability Times: </label><input type=text id name=availability" +
+    "></textarea><br><br></div></div><div class=form-group><div class=row><label class=form-label for>Availability Times: </label><input class=form-control type=text id name=availability" +
     marko_attr("value", input.volunteer.availability) +
-    "><br><br><label for>Address: </label><input type=text id=address name=address placeholder=\"house# street zip state\"" +
+    "><br><br></div></div><div class=form-group><div class=row><label class=form-label for>Address: </label><input class=form-control type=text id=address name=address placeholder=\"house# street zip state\"" +
     marko_attr("value", input.volunteer.address) +
-    "><br><br><label for>Phone #: </label><input type=text id=phoneNumber name=phone placeholder=##########" +
+    "><br><br></div></div><div class=form-group><div class=row><label class=form-label for>Phone #: </label><input class=form-control type=text id=phoneNumber name=phone placeholder=##########" +
     marko_attr("value", input.volunteer.phone) +
-    "><br><br><label for=email>Email: </label><input type=text id=email name=email placeholder=example@exaple.com" +
+    "><br><br></div></div><div class=form-group><div class=row><label class=form-label for=email>Email: </label><input class=form-control type=text id=email name=email placeholder=example@exaple.com" +
     marko_attr("value", input.volunteer.email) +
-    "><br><br><label for=educationalBackground>Edcuational Background: </label><textarea rows=3 cols=40 name=education id=educationalBackground" +
+    "><br><br></div></div><div class=form-group><div class=row><label class=form-label for=educationalBackground>Edcuational Background: </label><textarea class=form-control rows=3 cols=40 name=education id=educationalBackground" +
     marko_attr("value", input.volunteer.education) +
-    "></textarea><br><br><label for>Current Licenses: </label><input type=text id name=licenses" +
+    "></textarea><br><br></div></div><div class=form-group><div class=row><label class=form-label for>Current Licenses: </label><input class=form-control type=text id name=licenses" +
     marko_attr("value", input.volunteer.licenses) +
-    "><br><br><label for>Emergency Contact Name: </label><input type=text id name=emergencyname" +
+    "><br><br></div></div><div class=form-group><div class=row><label class=form-label for>Emergency Contact Name: </label><input class=form-control type=text id name=emergencyname" +
     marko_attr("value", input.volunteer.emergencyname) +
-    "><br><br><label for>Emergency Contact Phone #: </label><input type=text id=emergencyPhoneNumber name=emergencyphone placeholder=##########" +
+    "><br><br></div></div><div class=form-group><div class=row><label class=form-label for>Emergency Contact Phone #: </label><input class=form-control type=text id=emergencyPhoneNumber name=emergencyphone placeholder=##########" +
     marko_attr("value", input.volunteer.emergencyphone) +
-    "><br><br><div class=form-group><div class=row><label for>Emergency Contact Email: </label><input type=text id=emergencyEmail name=emergencyemail placeholder=example@exaple.com" +
+    "><br><br></div></div><div class=form-group><div class=row><label class=form-label for>Emergency Contact Email: </label><input class=form-control type=text id=emergencyEmail name=emergencyemail placeholder=example@exaple.com" +
     marko_attr("value", input.volunteer.emergencyemail) +
-    "><br><br></div></div><div class=form-group><div class=row><label for>Emergency Contact Address: </label><input type=text id=emergencyAddress name=emergencyaddress placeholder=\"house# street zip state\"" +
+    "><br><br></div></div><div class=form-group><div class=row><label class=form-label for>Emergency Contact Address: </label><input class=form-control type=text id=emergencyAddress name=emergencyaddress placeholder=\"house# street zip state\"" +
     marko_attr("value", input.volunteer.emergencyaddress) +
-    "><br><br></div></div> <div class=form-group><div class=row><label>Copy of Drivers License on File: </label><select class=form-control name=dlfile" +
+    "><br><br></div></div> <div class=form-group><div class=row><label class=form-label>Copy of Drivers License on File: </label><select class=form-control name=dlfile" +
     marko_attr("value", input.volunteer.dlfile) +
-    "><option value=true>True</option><option value=false>False</option></select></div></div><div class=form-group><div class=row><label>Copy of SSN on file: </label><select class=form-control name=ssfile" +
+    "><option value=true>True</option><option value=false>False</option></select></div></div><div class=form-group><div class=row><label class=form-label>Copy of SSN on file: </label><select class=form-control name=ssfile" +
     marko_attr("value", input.volunteer.ssfile) +
-    "><option value=true>True</option><option value=false>False</option></select></div></div><div class=form-group><div class=row><label>Volunteers Approval Status: </label><select class=form-control name=approval" +
+    "><option value=true>True</option><option value=false>False</option></select></div></div><div class=form-group><div class=row><label class=form-label>Volunteers Approval Status: </label><select class=form-control name=approval" +
     marko_attr("value", input.volunteer.approval) +
-    "><option value=true>Approved</option><option value=false>Denied</option></select></div></div><div class=form-group><div class=row><input type=submit id=submit value=Submit></div></div></form><input type=button value=Cancel id=cancel><input type=button value=\"log out\" id=logOut onclick(\"logOut\")>");
+    "><option value=true>Approved</option><option value=false>Denied</option></select></div></div><div class=form-group><div class=row><div class=col-2 style=\"padding-left: 0;\"><input type=submit id=submit value=Submit></div><div class=col-2 style=\"padding-left: 0;\"><input type=button value=Cancel id=cancel></div></div></div></div></div></form><input type=button value=\"log out\" id=logOut onclick(\"logOut\")>");
 
   init_components_tag({}, out);
 
-  await_reorderer_tag({}, out, __component, "100");
+  await_reorderer_tag({}, out, __component, "132");
 
   _preferred_script_location_tag({}, out);
 
